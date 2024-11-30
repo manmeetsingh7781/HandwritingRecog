715d65f87cdd1065d789720b45a939c4e32a1b37
Manmeet Singh <35901948+manmeetsingh7781@users.noreply.github.com>
2024-11-30 00:36:26 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request
 from mnist import get_image, get_model_initial_prints
 import base64
+import os
 
+# Define the path to the images folder
+IMAGE_FOLDER = "/opt/render/project/images"
+
+# Create the directory if it doesn't exist
+os.makedirs(IMAGE_FOLDER, exist_ok=True)
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
 
@@ -26,13 +32,13 @@ def process_image():
         # removing the header of image
         image_data = base64.b64decode(image.split("base64,")[1])
         # write to file
-        path = "./images/image.png"
+        path = os.path.join(IMAGE_FOLDER, "image.png")
         with open(path, "wb") as f:
             f.write(image_data)
             f.close()
 
         return {
-            "output": get_image(path.split("/")[2])
+            "output": get_image(path)
         }
     return {
         "output": "Error, unable to save image"
